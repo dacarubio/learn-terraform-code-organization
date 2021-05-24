326c5c20246179ea97ac0aa950ec6098ef966b47
dacarubio <38148369+dacarubio@users.noreply.github.com>
2021-05-25 10:46:19 +1200

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -15,48 +15,8 @@ resource "random_pet" "petname" {
   separator = "-"
 }
 
-# resource "aws_s3_bucket" "dev" {
-#   bucket = "${var.dev_prefix}-${random_pet.petname.id}"
-#   acl    = "public-read"
-
-#   policy = <<EOF
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Sid": "PublicReadGetObject",
-#             "Effect": "Allow",
-#             "Principal": "*",
-#             "Action": [
-#                 "s3:GetObject"
-#             ],
-#             "Resource": [
-#                 "arn:aws:s3:::${var.dev_prefix}-${random_pet.petname.id}/*"
-#             ]
-#         }
-#     ]
-# }
-# EOF
-
-#   website {
-#     index_document = "index.html"
-#     error_document = "error.html"
-
-#   }
-#   force_destroy = true
-# }
-
-# resource "aws_s3_bucket_object" "dev" {
-#   acl          = "public-read"
-#   key          = "index.html"
-#   bucket       = aws_s3_bucket.dev.id
-#   content      = file("${path.module}/assets/index.html")
-#   content_type = "text/html"
-
-# }
-
-resource "aws_s3_bucket" "prod" {
-  bucket = "${var.prod_prefix}-${random_pet.petname.id}"
+resource "aws_s3_bucket" "dev" {
+  bucket = "${var.dev_prefix}-${random_pet.petname.id}"
   acl    = "public-read"
 
   policy = <<EOF
@@ -71,7 +31,7 @@ resource "aws_s3_bucket" "prod" {
                 "s3:GetObject"
             ],
             "Resource": [
-                "arn:aws:s3:::${var.prod_prefix}-${random_pet.petname.id}/*"
+                "arn:aws:s3:::${var.dev_prefix}-${random_pet.petname.id}/*"
             ]
         }
     ]
@@ -86,11 +46,50 @@ EOF
   force_destroy = true
 }
 
-resource "aws_s3_bucket_object" "prod" {
+resource "aws_s3_bucket_object" "dev" {
   acl          = "public-read"
   key          = "index.html"
-  bucket       = aws_s3_bucket.prod.id
+  bucket       = aws_s3_bucket.dev.id
   content      = file("${path.module}/assets/index.html")
   content_type = "text/html"
-
 }
+
+# resource "aws_s3_bucket" "prod" {
+#   bucket = "${var.prod_prefix}-${random_pet.petname.id}"
+#   acl    = "public-read"
+
+#   policy = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "PublicReadGetObject",
+#             "Effect": "Allow",
+#             "Principal": "*",
+#             "Action": [
+#                 "s3:GetObject"
+#             ],
+#             "Resource": [
+#                 "arn:aws:s3:::${var.prod_prefix}-${random_pet.petname.id}/*"
+#             ]
+#         }
+#     ]
+# }
+# EOF
+
+#   website {
+#     index_document = "index.html"
+#     error_document = "error.html"
+
+#   }
+#   force_destroy = true
+# }
+
+# resource "aws_s3_bucket_object" "prod" {
+#   acl          = "public-read"
+#   key          = "index.html"
+#   bucket       = aws_s3_bucket.prod.id
+#   content      = file("${path.module}/assets/index.html")
+#   content_type = "text/html"
+
+# }
